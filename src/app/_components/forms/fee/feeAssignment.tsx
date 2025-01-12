e0d104314977f01b90cfe84d190327dad4598fcb
h470h <hassaanazam7@gmail.com>
2025-01-12 18:12:27 +0500

    FeeAssignment

--- src/app/_components/forms/fee/feeAssignment.tsx
+++ src/app/_components/forms/fee/feeAssignment.tsx
@@ -11,8 +11,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import {
   Select,
   SelectContent,
